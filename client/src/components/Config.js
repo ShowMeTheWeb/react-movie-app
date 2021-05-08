c59d91ec263fc27177764f24e20c19bf0c558067
ShowMeTheWeb <vaccine1220@naver.com>
2021-05-08 11:40:26 +0900

    react-movie-app

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'f54b413205df8c9c161500e9b261dfbf';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
